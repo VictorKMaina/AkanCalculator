d5f622b9507215b2ce53cffa49a43d947ab20cb2
VictorKMaina <vk13runic@gmail.com>
2020-02-14 16:03:36 +0300

    add different steps to app

--- js/scripts.js
+++ js/scripts.js
@@ -9,38 +9,35 @@
 //   return d.getDay();
 // }
 
-var check = function () {
-  var d = new Date();
-  var userDate = document.getElementById("date").value;
-  var userMonth = document.getElementById("month").value;
-  var userYear  = document.getElementById("year").value;
-  d.setDate(parseInt(userDate));
-  d.setMonth(parseInt(userMonth)-1);
-  d.setFullYear(parseInt(userYear));
-  return d.getDay();
+var check = function() {
+    var d = new Date();
+    var userDate = document.getElementById("date").value;
+    var userMonth = document.getElementById("month").value;
+    var userYear = document.getElementById("year").value;
+    d.setDate(parseInt(userDate));
+    d.setMonth(parseInt(userMonth) - 1);
+    d.setFullYear(parseInt(userYear));
+    return d.getDay();
 }
 
 function output() {
-  if (check() === 0) {
-    return("Sunday");
-  } else if (check() === 1){
-    return("Monday");
-  } else if (check() === 2){
-    return("Tuesday");
-  } else if (check() === 3){
-    return("Wednesday");
-  } else if (check() === 4){
-    return("Thursday");
-  } else if (check() === 5){
-    return("Friday");
-  } else if (check() === 6){
-    return("Saturday");
-  }
-}
-function entry() {
-  document.getElementById("entry").innerHTML = output();
+    if (check() === 0) {
+        return ("Sunday");
+    } else if (check() === 1) {
+        return ("Monday");
+    } else if (check() === 2) {
+        return ("Tuesday");
+    } else if (check() === 3) {
+        return ("Wednesday");
+    } else if (check() === 4) {
+        return ("Thursday");
+    } else if (check() === 5) {
+        return ("Friday");
+    } else if (check() === 6) {
+        return ("Saturday");
+    }
 }
 
-var trial = document.getElementById("trial").value;
-var res = trial.split("-");
-console.log(trial);
+function entry() {
+    document.getElementById("entry").innerHTML = output();
+}
